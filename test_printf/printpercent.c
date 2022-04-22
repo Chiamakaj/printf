b2679a0dbe73bb35928659cbc30c5cc51fd3c9c8
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-21 17:16:55 -0700

    adding test files

---
+++ test_printf/printpercent.c
@@ -0,0 +1,14 @@
+#include main.h"
+/**
+* printpercent - print %
+* @format: format
+* @pa: va_list
+* return: number of characters printed
+*/
+int printpercent(char *format, va_list pa)
+{
+(void)format;
+(void)pa;
+_putchar ("%");
+return (1);
+}
